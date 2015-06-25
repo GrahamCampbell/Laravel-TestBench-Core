830ac66d8facb7b67c952ff3ed2807ac47f6c4b1
Graham Campbell <graham@alt-three.com>
2015-06-25 14:38:53 +0100

    Updated email

--- src/HelperTrait.php
+++ src/HelperTrait.php
@@ -3,7 +3,7 @@
 /*
  * This file is part of Laravel TestBench Core.
  *
- * (c) Graham Campbell <graham@cachethq.io>
+ * (c) Graham Campbell <graham@alt-three.com>
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
@@ -16,7 +16,7 @@ use InvalidArgumentException;
 /**
  * This is the helper trait.
  *
- * @author Graham Campbell <graham@cachethq.io>
+ * @author Graham Campbell <graham@alt-three.com>
  * @author Joseph Cohen <joseph.cohen@dinkbit.com>
  */
 trait HelperTrait
