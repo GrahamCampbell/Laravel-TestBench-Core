eb8a72c95677500f715133f14b56935f3b249af8
Graham Campbell <graham@alt-three.com>
2017-07-27 15:59:13 -0400

    Added bc layer

---
+++ tests/BCTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * This file is part of Laravel TestBench.
+ *
+ * (c) Graham Campbell <graham@alt-three.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace GrahamCampbell\Tests\TestBenchCore;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * This is the helpers test class.
+ *
+ * @author Graham Campbell <graham@alt-three.com>
+ */
+class BCTest extends TestCase
+{
+    public function testLegacyExists()
+    {
+        $this->assertTrue(class_exists(\PHPUnit_Framework_TestCase::class));
+    }
+}
