3216e57016f52c88129809d8cdcae46f5ec8302d
Graham Campbell <graham@alt-three.com>
2017-12-31 17:24:16 +0000

    Removed bc layer

--- src/bc.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-if (class_exists(PHPUnit\Framework\TestCase::class) && !class_exists(PHPUnit_Framework_TestCase::class)) {
-    class_alias(PHPUnit\Framework\TestCase::class, PHPUnit_Framework_TestCase::class);
-}
